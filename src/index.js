36f12e2ea45ae8b803ebad9b983ac1a51b819976
HelgagleH <olyyyya@ukr.net>
2023-06-21 23:44:16 +0300

    Removed logos, added index.html, App.css, index.js, Search.js, Weather.css, Weather.js

--- src/index.js
+++ src/index.js
@@ -4,14 +4,36 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./App.css";
+import Weather from "./Weather";
+
+const rootElement = document.getElementById("root");
+const root = createRoot(rootElement);
+
 root.render(
   <React.StrictMode>
-    <App />
+    <div className="App">
+      <Weather />
+
+      <div className="bottom-link">
+        <a
+          href="https://github.com/HelgagleH/first-repository"
+          target="_blank"
+          rel="noreferrer"
+        >
+          Open-source code
+        </a>
+        <span className="signature"> by Olha Dashchynska</span>
+      </div>
+    </div>
   </React.StrictMode>
 );
-
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
+
+
+
