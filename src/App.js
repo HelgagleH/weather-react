160fa2f80a62a855548248f8b452643a06331790
HelgagleH <olyyyya@ukr.net>
2023-06-22 21:31:37 +0300

    Modified index.js, App.js

--- src/App.js
+++ src/App.js
@@ -1,25 +1,45 @@
 import './App.css';
 import logo from './logo.svg';
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./App.css";
+// import Weather from "./Weather";
 
 function App() {
   return (
+    // <div className="App">
+    //   <header className="App-header">
+    //     <img src={logo} className="App-logo" alt="logo" />
+    //     <p>
+    //       Edit <code>src/App.js</code> and save to reload.
+    //     </p>
+    //     <a
+    //       className="App-link"
+    //       href="https://reactjs.org"
+    //       target="_blank"
+    //       rel="noopener noreferrer"
+    //     >
+    //       Learn React
+    //     </a>
+    //   </header>
+    // </div>
+
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+      {/* <Weather /> */}
+
+      <div className="bottom-link">
         <a
-          className="App-link"
-          href="https://reactjs.org"
+          href="https://github.com/HelgagleH/first-repository"
           target="_blank"
-          rel="noopener noreferrer"
+          rel="noreferrer"
         >
-          Learn React
+          Open-source code
         </a>
-      </header>
+        <span className="signature"> by Olha Dashchynska</span>
+      </div>
     </div>
   );
 }
 
 export default App;
+
